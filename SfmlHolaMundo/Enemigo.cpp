e9956965653764d335b64454ca7f3ba199f6f349
LuisDev99 <luisgpr99@gmail.com>
2018-03-23 14:59:43 -0600

    100 % Terminado

--- SfmlHolaMundo/Enemigo.cpp
+++ SfmlHolaMundo/Enemigo.cpp
@@ -1,21 +1,41 @@
 
 #include "Enemigo.h"
+#include  <SFML/Window.hpp>
+#include <Windows.h>
+#include <SFML/Window/Mouse.hpp>
 #include <iostream>
+#include "Enemigo.h"
+#include <random>
+#include <functional>
+#include <cstdlib>
+#include <cmath>
+#include "Menu.h"
 
 static bool hit = false;
 
+
 using namespace sf;
-Enemigo::Enemigo()
+using namespace std;
+
+
+Enemigo::Enemigo(std::_Binder < std::_Unforced, std::uniform_int_distribution<int> &, std::reference_wrapper<std::default_random_engine>>random, sf::Color color)
 {
+	distribution = std::uniform_int_distribution<int>(-16, 16);
+	direction = sf::Vector2f(random(), random());
+	velocity = 20;
+	update_ms = sf::seconds(1.f / 30.f);
+
+	ball = sf::CircleShape(ball_radius - 4);
+	ball.setOutlineThickness(4);
+	ball.setOutlineColor(sf::Color::Black);
+	ball.setFillColor(color);
+	ball.setOrigin(ball_radius / 2, ball_radius / 2);
+	ball.setPosition(window_width / 2, window_height / 2);
+
 }
 
-Enemigo::Enemigo(sf::Color color, int x, int y, sf::RectangleShape *&rectangle)
-{
-	rectangle->setSize(sf::Vector2f(100, 50));
-	/*rectangle.setOutlineColor(sf::Color::Red);*/
-	rectangle->setFillColor(color);
-	rectangle->setOutlineThickness(2);
-	rectangle->setPosition(x, y);
+Enemigo::Enemigo() {
+
 }
 
 
@@ -36,138 +56,10 @@ bool Enemigo::checkShotCircle(sf::RenderWindow &window, sf::CircleShape &shape) 
 
 	if (mousePosicion.x >= (posicion.x - 260) && mousePosicion.x <= (posicion.x + 260) && mousePosicion.y >= (posicion.y - 260) && mousePosicion.y <= (posicion.y + 260) && hit == false) {
 		shape.setFillColor(Color::Red);
-
-		/*health -= 10;
-		if (health == 0) {
-			shape.setPosition(newX, newY);
-			health = 40;
-			respawns++;
-		}
-		if (respawns == 4) {
-			std::cout << "DEAD" << std::endl;
-			isDead = true;
-		}
-
-		alreadyHit = true;*/
-
-		/*std::cout << posicion.x << std::endl;
-		std::cout << mousePosicion.x <<  std::endl;
-		std::cout << "Hit" << std::endl;*/
-
 		return true;
 	}
-	//std::cout << posicion.x << std::endl;
-	//std::cout << posicion.x << std::endl;
-	//std::cout << mousePosicion.x << std::endl;
-
-	//std::cout << posicion.y << std::endl;
-	//std::cout << mousePosicion.y << std::endl;
-	//std::cout << "Hit" << std::endl;
 	return false;
 }
-
-
-bool Enemigo::checkShot(sf::RenderWindow &window, sf::CircleShape &shape, sf::RectangleShape *&rectangle) {
-
-	auto positionReader = rectangle->getPosition();
-	auto mousePosition = Mouse::getPosition(window);
-	auto newX = rand() % ScreenWidth + 1;
-	auto newY = rand() % ScreenHeight + 1;
-
-	/*system("cls");
-	std::cout << "Mouse X: " << mousePosition.x << std::endl;
-	std::cout << "Mouse Y: " << mousePosition.y << std::endl;
-	std::cout << "Rect X: " << positionReader.x << std::endl;
-	std::cout << "Rect Y: " << positionReader.y << std::endl;
-	std::cout << "New X: " << newX << "New Y: " << newY << "Health: "<<health <<std::endl;*/
-
-
-	if (mousePosition.x >= (positionReader.x - 20) && mousePosition.x <= (positionReader.x + 100) && mousePosition.y >= (positionReader.y - 50) && mousePosition.y <= (positionReader.y + 50) && alreadyHit == false) {
-		shape.setFillColor(Color::Red);
-		health -= 10;
-		if (health == 0) {
-			rectangle->setPosition(newX, newY);
-			health = 40;
-			respawns++;
-		}
-		if (respawns == 4) {
-			std::cout << "DEAD" << std::endl;
-			isDead = true;
-			return isDead;
-		}
-
-		alreadyHit = true;
-		//std::cout << "Hit" << std::endl;
-	}
-
-	return isDead;
-}
-
-void Enemigo::checkMouseHover(sf::RectangleShape *&rectangle, sf::RenderWindow &window, sf::CircleShape &shape) {
-	auto positionReader = rectangle->getPosition();
-	auto mousePosition = sf::Mouse::getPosition(window);
-
-	if (isBeingHit(positionReader, mousePosition)) {
-		shape.setFillColor(sf::Color::Blue);
-	}
-	else {
-		shape.setFillColor(sf::Color::Green);
-	}
-
-}
-
-void Enemigo::move(sf::RectangleShape *&rectangle) {
-	auto oldPosition = rectangle->getPosition();
-	double newX = oldPosition.x, newY = oldPosition.y;
-
-
-	//Caso 1 el enemigo se movera para la izquierda
-	//Caso 2 el enemigo se movera para arriba
-	//Caso 3 el enemigo se movera por los dos sentidos
-
-	switch (rand() % 3 + 1) {
-	case 1:
-		if (oldPosition.x < ScreenWidth - 100) {
-			newX = oldPosition.x + rand() % 1 + 0.1;
-			newY = oldPosition.y;
-		}
-		break;
-	case 2:
-		if (oldPosition.y < ScreenHeight - 50) {
-			newX = oldPosition.x;
-			newY = oldPosition.y - 0.1 - rand() % 1 + 0.1;
-		}
-		else if (oldPosition.y >= ScreenHeight - 50) {
-			newX = oldPosition.x;
-			newY = oldPosition.y + 0.1 + rand() % 1 + 0.1;
-		}
-		break;
-	case 3:
-		if (oldPosition.x < ScreenWidth - 100 && oldPosition.y < ScreenHeight - 50 && isGoingUp == false) {
-			newX = oldPosition.x + +rand() % 2 + 0.1;
-			newY = oldPosition.y + rand() % 2 + 0.1;
-		}
-		else {
-			isGoingUp = true;
-			newX = oldPosition.x - rand() % 2 + 0.1;
-			newY = oldPosition.y - rand() % 3 + 0.1;
-			if (oldPosition.y < 0)
-				isGoingUp = false;
-			else if (oldPosition.x > 800) {
-				isGoingUp = true;
-			}
-
-		}
-		//
-		break;
-	}
-
-	/*std::cout <<"X: " << oldPosition.x << std::endl;
-	std::cout << "Y: " << oldPosition.y << std::endl;*/
-
-	rectangle->setPosition(newX, newY);
-}
-
 
 
 void Enemigo::restart()
@@ -175,4 +67,26 @@ void Enemigo::restart()
 	health = 20;
 	respawns = 0;
 	isDead = false;
+}
+
+void Enemigo::moveCircle()
+{
+	const auto pos = ball.getPosition();
+	const auto delta = update_ms.asSeconds() * velocity;
+	sf::Vector2f new_pos(pos.x + direction.x * delta, pos.y + direction.y * delta);
+
+	if (new_pos.x - ball_radius < 0) {
+		direction.x *= -1;
+		new_pos.x = 0 + ball_radius;
+	} else if (new_pos.x + ball_radius >= window_width) {
+		direction.x *= -1;
+		new_pos.x = window_width - ball_radius;
+	} else if (new_pos.y - ball_radius < 0) {
+		direction.y *= -1;
+		new_pos.y = 0 + ball_radius;
+	} else if (new_pos.y + ball_radius >= window_height) {
+		direction.y *= -1;
+		new_pos.y = window_height - ball_radius;
+	}
+	ball.setPosition(new_pos);
 }
--- SfmlHolaMundo/Enemigo.h
+++ SfmlHolaMundo/Enemigo.h
@@ -5,6 +5,12 @@
 #include <Windows.h>
 #include <SFML/Window/Mouse.hpp>
 #include <iostream>
+#include "Enemigo.h"
+#include <random>
+#include <functional>
+#include <cstdlib>
+#include <cmath>
+#include "Menu.h"
 
 #define ScreenWidth 1000
 #define ScreenHeight 400
@@ -12,19 +18,34 @@ class Enemigo
 {
 public:
 	Enemigo();
-	Enemigo(sf::Color, int, int, sf::RectangleShape *&);
+	Enemigo(std::_Binder < std::_Unforced, std::uniform_int_distribution<int> &, std::reference_wrapper<std::default_random_engine>>random, sf::Color);
+	
+
+	//Game
+	std::random_device seed_device;
+	std::default_random_engine engine;
+	std::uniform_int_distribution<int> distribution;
+	sf::CircleShape ball;
+	sf::Vector2f direction;
+	sf::Clock clock;
+	sf::Time elapsed;
+	sf::Time update_ms;
+
+	float velocity;
+	const int window_width = 1000;
+	const int window_height = 400;
+	const float ball_radius = 30.f;
+	const int bpp = 32;
 
 	int health = 40;
 	int respawns = 0;
 	bool isGoingUp = false;
 	bool alreadyHit = false;
 	bool isDead = false;
-	bool checkShot(sf::RenderWindow &, sf::CircleShape&, sf::RectangleShape *&);
-	void checkMouseHover(sf::RectangleShape *&, sf::RenderWindow &, sf::CircleShape &);
-	void move(sf::RectangleShape *&);
 	//void crearPelota();
 	static bool checkShotCircle(sf::RenderWindow &, sf::CircleShape &);
 	void restart();
+	void moveCircle();
 
 	~Enemigo();
 };
